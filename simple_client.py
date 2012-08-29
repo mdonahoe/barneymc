d30b2f8d7f0d50cfe04d49fc92801b65b9c9a7d2
Matt Donahoe <matt@serioussandbox.com>
2012-08-29 13:48:40 -0700

    diamond detector

--- simple_client.py
+++ simple_client.py
@@ -1,9 +1,11 @@
 import asyncore
 from vector import Vector
 from random import random
+from math import floor
 import re
 ptypes = {}
 
+import chunk
 from barneymc.protocol.packet import *
 from barneymc.protocol import bound_buffer
 from barneymc.net import client
@@ -17,6 +19,11 @@ dvec_from_data = lambda d: Vector(*extractor(dxyz, d))
 
 class Player(object):
     all = dict()
+    @staticmethod
+    def get_by_name(username):
+        for p in Player.all.values():
+            if p.name == username: return p
+
     def __init__(self, eid, name, pos):
         self.eid = eid
         self.name = name
@@ -50,10 +57,13 @@ class SimpleClient(client.Client):
     spawned = False
     last_pos = None
     me = None
-    target_player = 'mdonahoe'
+    target_player = 'None'
+    target = None
+    announce_delay = 0
+    digging = False
     def __init__(self, **settings):
         self.handlers = {
-            0x00: self.reflect, #Echos the packet back to the server
+            0x00: self.repeat, #Echos the packet back to the server
             0x01: self.parselogin,
             0x03: self.get_chat,
             0x0d: self.positionandlook,
@@ -61,6 +71,7 @@ class SimpleClient(client.Client):
             0x21: self.rel_update_player,
             0x14: self.create_player,
             0x22: self.update_player, #teleport
+            #0x35: self.block_change,
             0xfd: self.login,
             0xfa: self.print_plugin,
             0xff: self.disconnect}
@@ -71,7 +82,44 @@ class SimpleClient(client.Client):
 
         data = dict([(k,self.settings[k]) for k in ('username', 'host', 'port')])
         data['protocol'] = 39
-        self.send_packet(Packet(ident=0x02, data=data))
+        self.act(0x02, data)
+
+    def act(self, pid, data):
+        self.send_packet(Packet(ident=pid, data=data))
+
+    def repeat(self, packet):
+        self.reflect(packet)
+        #self.destroy()
+        self.move()
+
+    def block_change(self, packet):
+        print 'block changed', packet.data
+        data = packet.data
+        if data['id'] != 0:
+            # block not deleted
+            return
+        pos = vec_from_data(data)
+        self.target = pos + Vector(.5,.1,.5)
+        self.move()
+
+
+    def destroy(self):
+        # delete the block beneath the bot
+        # simplest thing i could think of
+        p = self.me.pos
+        x = floor(p.x)
+        y = floor(p.y - 1)
+        z = floor(p.z)
+        if self.digging:
+            s = 2
+            print 'done digging'
+            self.digging = False
+        else:
+            s = 0
+            print 'starting to dig'
+            self.digging = True
+        data = dict(x=x,y=y,z=z,status=s, face=1)
+        self.act(0x0E, data)
 
     def create_player(self, packet):
         # named player
@@ -92,8 +140,20 @@ class SimpleClient(client.Client):
         if username == 'bot':
             print 'skip self'
             return
-        self.chat('i hate ' + username)
-        self.target_player = username
+        # report interesting
+        p = Player.get_by_name(username)
+        self.detect(p, 30)
+
+    def detect(self, p, minimum=8):
+        if not p.pos: return
+        vs = [Vector(*v) for v in chunk.interest]
+        d,v = min(((v - p.pos).mag(), v) for v in vs)
+        if d < minimum:
+            if self.announce_delay > 0:
+                self.announce_delay-=1
+                return
+            self.announce_delay = 10
+            self.chat('%s from %s' %(int(d), p.name))
 
     def attack(self, player):
         data = dict()
@@ -108,6 +168,24 @@ class SimpleClient(client.Client):
     def login(self, packet):
         print 'logging in'
         self.send_packet(Packet(ident=0xcd, data=dict(payload=0)))
+
+    def move(self, pos=None):
+        if pos:
+            self.me.pos = pos
+        elif self.target:
+            dv = self.target - self.me.pos
+            if dv.mag() > .5:
+                self.me.move(dv.norm())
+            else:
+                self.target_reached()
+        self.act(0x0D, self.me.get_position_and_look())
+
+    def target_reached(self):
+        print 'target reached:', self.target
+        self.digging = False
+        self.target = None
+        self.destroy()
+        self.destroy()
 
     def parselogin(self, packet):
         print 'logged in'
@@ -125,10 +203,7 @@ class SimpleClient(client.Client):
         pos = (1.0/32) * vec_from_data(packet.data)
         p.pos = pos
         if p.name == self.target_player:
-            dv = (p.pos - self.me.pos).norm()
-            self.me.move(dv)
-            print p, self.me
-            self.send_packet(Packet(ident=0x0D, data=self.me.get_position_and_look()))
+            self.target = p.pos
 
     def rel_update_player(self, packet):
         entity = packet.data['entity_id']
@@ -137,12 +212,11 @@ class SimpleClient(client.Client):
         p = Player.all[entity]
         dpos = (1.0/32) * dvec_from_data(packet.data)
         p.move(dpos)
+        self.detect(p)
         if p.name == self.target_player:
+            self.target = p.pos
             dv = (p.pos - self.me.pos).norm()
-            self.me.move(dv)
-            print p, self.me
-            self.send_packet(Packet(ident=0x0D, data=self.me.get_position_and_look()))
-            if dv.mag() < 4:
+            if 0 and dv.mag() < 4:
                 print 'attack!'
                 self.attack(p)
 
@@ -177,3 +251,4 @@ if __name__ == '__main__':
     x.sort()
     for v,k in x:
         print k,v
+    #chunk.block_stats()
