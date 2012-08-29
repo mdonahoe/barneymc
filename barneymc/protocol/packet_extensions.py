90188d3ff619704bcb851d6d6f32c8430791965f
Matt Donahoe <matt@serioussandbox.com>
2012-08-29 13:48:22 -0700

    parse map chunk packets

--- barneymc/protocol/packet_extensions.py
+++ barneymc/protocol/packet_extensions.py
@@ -1,4 +1,5 @@
 from data_types import *
+import chunk
 
 extensions = {}
 def extension(ident):
@@ -15,7 +16,7 @@ class Extension17:
             packet.data['x2'] = unpack(bbuff, 'short')
             packet.data['y2'] = unpack(bbuff, 'short')
             packet.data['z2'] = unpack(bbuff, 'short')
-    
+
     @classmethod
     def encode_extra(self, packet):
         append = ''
@@ -31,14 +32,16 @@ class DestroyEntity:
         count = int(packet.data['entity_count'])
         for i in range(count):
             eid = unpack(bbuff, "int")
-    
+
 @extension(0x33)
 class Extension33:
     @classmethod
     def decode_extra(self, packet, bbuff):
         packet.data['data'] = unpack_array_fast(bbuff, 'ubyte', packet.data['data_size'])
         del packet.data["data_size"]
-    
+        chunk.extract_map(packet)
+
+
     @classmethod
     def encode_extra(self, packet):
         packet.data['data_size'] = len(packet.data['data'])
@@ -59,14 +62,14 @@ class Extension34:
                 'x':        (data >> 28) & 0xF}
             packet.data["blocks"].append(block)
         del packet.data["data_size"]
-    
+
     @classmethod
     def encode_extra(self, packet):
         packet.data['record_count']  = len(packet.data['blocks'])
         packet.data['data_size'] = 4 * len(packet.data['blocks'])
         append = ''
         for block in packet.data['blocks']:
-            append += pack('uint', 
+            append += pack('uint',
                 (block['metadata']  ) +
                 (block['type'] <<  4) +
                 (block['y']    << 16) +
@@ -78,15 +81,35 @@ class Extension34:
 class MapChunkBulk:
     @classmethod
     def decode_extra(self, packet, bbuff):
-        chunk_data = unpack_array_fast(bbuff, 'byte', packet.data['data_len'])
+        import zlib
+        L = packet.data['data_len']
+        chunk_data = zlib.decompress(bbuff.read(L))
+        """
+Each part is 10240 * n + 256 bytes.
+n is the number of sections in the current chunk
+(this is the number of flags set in the primary bitmap).
+10240 is the amount of bytes for each chunk without add bitmap,
+256 bytes are used for biomes.
+"""
+        j = 0
         for i in range(packet.data['chunk_count']):
-            # meta data! horray!
             cx = unpack(bbuff, 'int')
             cz = unpack(bbuff, 'int')
             primary_bitmap = unpack(bbuff, 'short')
             add_bitmap = unpack(bbuff, 'short')
-            
-        
+
+            # count the ones
+            n = bin(primary_bitmap).count('1')
+
+            # length of this chunk (wihout 'add' data)
+            k = 10240 * n + 256
+
+            # this chunk's data
+            d = chunk_data[j:j+k]
+            j+=k
+
+            chunk.chunky(cx,cz,primary_bitmap,d)
+
 
 @extension(0x3C)
 class Explosion:
@@ -102,14 +125,14 @@ class Explosion:
         unpack(bbuff, "float")
         unpack(bbuff, "float")
         unpack(bbuff, "float")
-    
+
     @classmethod
     def encode_extra(self, packet):
         packet.data['data_size'] = len(packet.data['blocks'])
         array = []
         for i in packet.data['blocks']:
             array += [i['x'], i['y'], i['z']]
-        
+
         return pack_array_fast('byte', array)
 
 @extension(0x68)
@@ -118,7 +141,7 @@ class Extension68:
     def decode_extra(self, packet, bbuff):
         packet.data["slots_data"] = unpack_array(bbuff, 'slot', packet.data["data_size"])
         del packet.data["data_size"]
-    
+
     @classmethod
     def encode_extra(self, packet):
         packet.data['data_size'] = len(packet.data['slots_data'])
@@ -131,7 +154,7 @@ class Extension82:
         packet.data["text"] = []
         for i in range(4):
             packet.data["text"].append(packet.data.pop("line_%s" % (i+1)))
-    
+
     @classmethod
     def encode_extra(self, packet):
         for i in range(4):
@@ -145,19 +168,19 @@ class Extension83:
     def decode_extra(self, packet, bbuff):
         packet.data["data"] = unpack_array_fast(bbuff, 'ubyte', packet.data['data_size'])
         del packet.data["data_size"]
-    
+
     @classmethod
     def encode_extra(self, packet):
         packet.data['data_size'] = len(packet.data['data'])
         return pack_array_fast('ubyte', packet.data['data'])
 
-@extension(0xFA)    
+@extension(0xFA)
 class ExtensionFA:
     @classmethod
     def decode_extra(self, packet, bbuff):
         packet.data["data"] = unpack_array_fast(bbuff, 'byte', packet.data['data_size'])
         del packet.data["data_size"]
-    
+
     @classmethod
     def encode_extra(self, packet):
         packet.data['data_size'] = len(packet.data['data'])
